4906b9b5f8f3779ff405f88e23edc5ed0dc811ba
German Galvis <german.galvis@zemogacom>
2013-04-01 18:17:19 -0500

    removing comments and fixing indentation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 (function(AppContext) {
-	// var users = [
-	// 	new User('Barney'),
-	// 	new User('Cartman'),
-	// 	new User('Sheldon')
-	// ];
-
-	// for (var i = 0, len = users.length; i < len; i++) {
-	// 	console.log(users[i].name);
-	// }
-
-	// localStorage.users = JSON.stringify(users);
 	AppContext.Router.startRouting();
 
 }(window));
